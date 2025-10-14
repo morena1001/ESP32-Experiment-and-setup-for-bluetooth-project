d7666d6a674d28d6889996a7f7bc94913a994956
morena1001 <josueflores15.jf@gmail.com>
2025-10-13 20:56:26 -0500

    Initial commit. Managed to solve COM port connectivity issues, and programmed a blinking LED program

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+#define LED 2
+
+void setup() {
+  Serial.begin (115200);
+  pinMode (LED, OUTPUT);
+}
+
+void loop() {
+  digitalWrite (LED, HIGH);
+  Serial.println ("LED is on");
+  delay (500);
+  
+  digitalWrite (LED, LOW);
+  Serial.println ("LED is off");
+  delay (500);
+}
