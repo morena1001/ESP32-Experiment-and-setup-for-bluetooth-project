74ab446a1662025c7753b00e813b3a26f6aacad5
morena1001 <josueflores15.jf@gmail.com>
2025-10-16 18:44:05 -0500

    Can now send commands to pause and play music. Will now try to do first part of the prototype, to control audio through button inputs

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,37 @@
 #include <Arduino.h>
+#include <BleKeyboard.h>
 
-#define LED 2
+BleKeyboard bleKeyboard ("Media Control");
 
-void setup() {
+const int pp_pin = 25; // GPIO pin for the pause/play button
+const int next_pin = 26; // GPIO pin for the next button
+const int prev_pin = 27; // GPIO pin for the previous button
+
+bool sent_connected_message = false;
+
+void setup () {
   Serial.begin (115200);
-  pinMode (LED, OUTPUT);
+
+  pinMode (pp_pin, INPUT);
+  pinMode (next_pin, INPUT);
+  pinMode (prev_pin, INPUT);
+
+  Serial.println ("Starting BLE work!");
+  bleKeyboard.begin ();
 }
 
-void loop() {
-  digitalWrite (LED, HIGH);
-  Serial.println ("LED is on");
-  delay (500);
-  
-  digitalWrite (LED, LOW);
-  Serial.println ("LED is off");
-  delay (500);
+void loop () {
+  if(bleKeyboard.isConnected () && !sent_connected_message) {
+    sent_connected_message = true;
+
+    Serial.println ("Device connected");
+    bleKeyboard.print ("Device connected");
+
+    // Serial.println ("Sending Play/Pause media key...");
+    // bleKeyboard.write (KEY_MEDIA_PLAY_PAUSE);
+  } else if (!bleKeyboard.isConnected ()) {
+    sent_connected_message = false;
+  }
+
+  Serial.println (digitalRead (pp_pin));
 }
