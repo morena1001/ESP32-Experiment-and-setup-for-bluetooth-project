f7f3bbb44aa3df557f6ec60095a6454d695459c7
morena1001 <josueflores15.jf@gmail.com>
2025-11-07 23:46:15 -0600

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,9 @@
 #include <Adafruit_Si4713.h>
 #include "Preferences.h"
 #include "BluetoothA2DPSink.h"
+#include "driver/dac.h"
 
-#define RESETPIN  12
+#define RESETPIN  13
 #define FMSTATION 9390
 
 Adafruit_Si4713 radio = Adafruit_Si4713 (RESETPIN);
@@ -37,7 +38,7 @@ int next_last_state = LOW;
 int prev_last_state = LOW; 
 
 void avrc_metadata_callback (uint8_t id, const uint8_t *text) {
-  // Serial.printf ("==> AVRC metadata rsp: attribute id 0x%x, %s\n", id, text);
+  Serial.printf ("==> AVRC metadata rsp: attribute id 0x%x, %s\n", id, text);
   // if (id == ESP_AVRC_MD_ATTR_PLAYING_TIME) {
   //   uint32_t playtime = String ((char*) text).toInt ();
   //   Serial.printf ("==> Playing time is %d ms (%d seconds)\n", playtime, (int) round (playtime/1000.0));
@@ -61,22 +62,25 @@ void avrc_rn_playstatus_callback (esp_avrc_playback_stat_t playback) {
 
 void setup () {
   Serial.begin (115200);
-  // Serial.println ("Starting radio");
+  Serial.println ("Starting radio");
 
   if (!radio.begin ()) {
-    // Serial.println ("Couldn't find radio");
+    Serial.println ("Couldn't find radio");
     while (!radio.begin ()) {
-      // Serial.println ("Couldn't find radio");
+      Serial.println ("Couldn't find radio");
     }
   }
-  // Serial.println ("Radio found");
+  Serial.println ("Radio found");
 
   radio.setTXpower(115);  // dBuV, 88-115 max
-  // Serial.print("\nTuning into "); 
-  // Serial.print(FMSTATION/100); 
-  // Serial.print('.'); 
-  // Serial.println(FMSTATION % 100);
-  // radio.tuneFM(FMSTATION); // 102.3 mhz
+  Serial.print("\nTuning into "); 
+  Serial.print(FMSTATION/100); 
+  Serial.print('.'); 
+  Serial.println(FMSTATION % 100);
+  radio.tuneFM(FMSTATION); // 102.3 mhz
+
+  dac_output_enable (DAC_CHANNEL_1);
+  dac_output_enable (DAC_CHANNEL_2);
 
   pinMode (pp_pin, INPUT);
   pinMode (next_pin, INPUT);
@@ -87,19 +91,20 @@ void setup () {
   a2dp_sink.set_avrc_rn_playstatus_callback (avrc_rn_playstatus_callback);
 
   a2dp_sink.start ("MyMusic", true);
-  // Serial.println ("Started bluetooth");
+  Serial.println ("Started bluetooth");
 
   preferences.begin ("connections", true);
   bool prev_init = preferences.isKey ("prev");
   if (prev_init) {
     esp_bd_addr_t prev_address;
     preferences.getBytes ("prev", prev_address, ESP_BD_ADDR_LEN);
-    // Serial.print ("Checking to see if ");
-    // Serial.print (a2dp_sink.to_str (prev_address));
-    // Serial.println (" Connects");
+    Serial.print ("Checking to see if ");
+    Serial.print (a2dp_sink.to_str (prev_address));
+    Serial.println (" Connects");
     if (a2dp_sink.connect_to (prev_address)) {
-      // Serial.print ("Reconnected to previously connected device: ");
-      // Serial.println (a2dp_sink.to_str (prev_address));
+      Serial.print ("Reconnected to previously connected device: ");
+      Serial.println (a2dp_sink.to_str (prev_address));
+      a2dp_sink.set_volume (100);
     }
   }
   preferences.end ();
@@ -124,10 +129,10 @@ void loop () {
       if (pp_state == HIGH) {
         is_active = !is_active;
         if (is_active) {
-          // Serial.println ("play");
+          Serial.println ("play");
           a2dp_sink.play ();
         } else {
-          // Serial.println ("pause");
+          Serial.println ("pause");
           a2dp_sink.pause ();
         }
         // bleKeyboard.write (KEY_MEDIA_PLAY_PAUSE);
@@ -140,7 +145,7 @@ void loop () {
       next_state = next_reading;
       
       if (next_state == HIGH) {
-        // Serial.println ("Next");
+        Serial.println ("Next");
         a2dp_sink.next ();
         // bleKeyboard.write (KEY_MEDIA_NEXT_TRACK);
       }
@@ -152,7 +157,7 @@ void loop () {
       prev_state = prev_reading;
       
       if (prev_state == HIGH) {
-        // Serial.println ("Previous");
+        Serial.println ("Previous");
         a2dp_sink.previous ();
         // bleKeyboard.write (KEY_MEDIA_PREVIOUS_TRACK);
       }
@@ -168,16 +173,17 @@ void loop () {
   if (a2dp_sink.is_connected () && !sent_connected_message) {
     sent_connected_message = true;
     sent_disconnected_message = false;
-    // Serial.println ("Device connected");
+    Serial.println ("Device connected");
+    a2dp_sink.set_volume (100);
 
     preferences.begin ("connections", false);
     preferences.putBytes ("prev", a2dp_sink.get_current_peer_address (), ESP_BD_ADDR_LEN);
-    // Serial.print ("Put address to memory: ");
-    // Serial.println (a2dp_sink.to_str (*(a2dp_sink.get_current_peer_address ())));
+    Serial.print ("Put address to memory: ");
+    Serial.println (a2dp_sink.to_str (*(a2dp_sink.get_current_peer_address ())));
     preferences.end ();
   } else if (!a2dp_sink.is_connected () && !sent_disconnected_message) {
     sent_disconnected_message = true;
     sent_connected_message = false;
-    // Serial.println ("Device disconnected");
+    Serial.println ("Device disconnected");
   }
 }
