551631e4fffccfdb2573d09eff9e5f4dafd205fa
morena1001 <josueflores15.jf@gmail.com>
2025-11-13 21:52:51 -0600

    Partition of font works well. The only issue is that the entire font is larger than the amount of space on the ESP32, so I will have to use the SD card slot on the screen to store the fonts and work with them. Hopefully it is sucessful because I will have to drop CJK support otherwise.

--- src/Screen_test.cpp
+++ src/Screen_test.cpp
@@ -27,8 +27,8 @@
 #include "Latin_Hiragana_24.h"
 #include "Unicode_Test_72.h"
 
-// #include "Unifont_Latin.h"
-// #include "Unifont_CJK_1.h"
+#include "Unifont_Latin.h"
+#include "Unifont_CJK_1.h"
 // #include "Unifont_CJK_2.h"
 // #include "Unifont_CJK_3.h"
 // #include "Unifont_CJK_4.h"
@@ -47,22 +47,39 @@ TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
 uint16_t bg = TFT_BLACK;
 uint16_t fg = TFT_WHITE;
 
-void Check_Location (uint32_t enc_byte) {
-    if (enc_byte >= 0xECB080 && enc_byte <= 0xED9FBF)       Serial.printf ("CJK 11\r\n");
-    else if (enc_byte >= 0xEBB080 && enc_byte <= 0xECB080)  Serial.printf ("CJK 10\r\n");
-    else if (enc_byte >= 0xEAB080 && enc_byte <= 0xEBB080)  Serial.printf ("CJK 9\r\n");
-    else if (enc_byte >= 0xE99080 && enc_byte <= 0xE9BFBF)  Serial.printf ("CJK 8\r\n");
-    else if (enc_byte >= 0xE89080 && enc_byte <= 0xE99080)  Serial.printf ("CJK 7\r\n");
-    else if (enc_byte >= 0xE79080 && enc_byte <= 0xE89080)  Serial.printf ("CJK 6\r\n");
-    else if (enc_byte >= 0xE69080 && enc_byte <= 0xE79080)  Serial.printf ("CJK 5\r\n");
-    else if (enc_byte >= 0xE59080 && enc_byte <= 0xE69080)  Serial.printf ("CJK 4\r\n");
-    else if (enc_byte >= 0xE49080 && enc_byte <= 0xE59080)  Serial.printf ("CJK 3\r\n");
-    else if (enc_byte >= 0xE39080 && enc_byte <= 0xE49080)  Serial.printf ("CJK 2\r\n");
-    else if (enc_byte >= 0xE2BA80 && enc_byte <= 0xE387BF)  Serial.printf ("CJK 1\r\n");
+#define FONT_ERROR  0
+#define CJK11       1
+#define CJK10       2
+#define CJK9        3
+#define CJK8        4
+#define CJK7        5
+#define CJK6        6
+#define CJK5        7
+#define CJK4        8
+#define CJK3        9
+#define CJK2        10
+#define CJK1        11
+#define LAT1        12
+#define LAT2        13
 
-    else if (enc_byte >= 0xE1B880 && enc_byte <= 0xE1BBB9)  Serial.printf ("Latin 2\r\n");
-    else if (enc_byte <= 0x00D4AF)                          Serial.printf ("Latin 1\r\n");
-    else                                                    Serial.printf ("COULD NOT FIND VALID LOCATION\r\n");
+uint8_t Check_Location (uint32_t enc_byte) {
+  if (enc_byte == 0)  return FONT_ERROR;
+  if (enc_byte >= 0xECB080 && enc_byte <= 0xED9FBF)       return CJK11;
+  else if (enc_byte >= 0xEBB080 && enc_byte <= 0xECB080)  return CJK10;
+  else if (enc_byte >= 0xEAB080 && enc_byte <= 0xEBB080)  return CJK9;
+  else if (enc_byte >= 0xE99080 && enc_byte <= 0xE9BFBF)  return CJK8;
+  else if (enc_byte >= 0xE89080 && enc_byte <= 0xE99080)  return CJK7;
+  else if (enc_byte >= 0xE79080 && enc_byte <= 0xE89080)  return CJK6;
+  else if (enc_byte >= 0xE69080 && enc_byte <= 0xE79080)  return CJK5;
+  else if (enc_byte >= 0xE59080 && enc_byte <= 0xE69080)  return CJK4;
+  else if (enc_byte >= 0xE49080 && enc_byte <= 0xE59080)  return CJK3;
+  else if (enc_byte >= 0xE39080 && enc_byte <= 0xE49080)  return CJK2;
+  else if (enc_byte >= 0xE2BA80 && enc_byte <= 0xE387BF)  return CJK1;
+
+  else if (enc_byte >= 0xE1B880 && enc_byte <= 0xE1BBB9)  return LAT2;
+  else if (enc_byte <= 0x00D4AF)                          return LAT1;
+  
+  return FONT_ERROR;
 }
 
 
@@ -81,7 +98,8 @@ void setup()
   fg = TFT_WHITE;
   bg = TFT_BLACK;
 
-  String rain = "p雨Àのr";
+  // String rain = "あp雨Ḅのr";
+  String rain = "あp";
   uint32_t utf8_val[10];
   uint8_t byte_counter = 0;
   bool in_encoding = false;
@@ -109,25 +127,93 @@ void setup()
   Serial.println ();
 
   for (uint32_t val : utf8_val) {
-    Serial.printf ("%X | ", val);
-    Check_Location (val);
+    Serial.printf ("%X | %d", val, Check_Location (val));
   }
 
   Serial.println ();
 
   
-//   tft.setTextColor(fg, bg);
-//   String test2 = "雨"; // Unicode 0x4EDD, 0x5000
-//   tft.loadFont(Unicode_Test_72);
-//   tft.setRotation(1);
-//   // Show all characters on screen with 2 second (2000ms) delay between screens
-// //   tft.showFont(2000); // Note: This function moves the cursor position!
-//   tft.fillScreen(bg);
-//   tft.setCursor(0,0);
-//   tft.setTextColor(TFT_CYAN, bg);
-//   tft.println(test2);
-// //   tft.println ("\nRYUSENKEI");
-//   tft.unloadFont();
+  tft.setTextColor(fg, bg);
+  tft.setRotation(1);
+  tft.fillScreen(bg);
+  tft.setCursor(0,0);
+  tft.setTextColor(TFT_CYAN, bg);
+  
+  for (uint8_t i = 0, j = 0; i <= rain.length (); j++) {
+    uint32_t val = utf8_val[j];
+    if (Check_Location (val) == LAT1) {
+      tft.loadFont (latin_1);
+      tft.println (rain.substring (i, i + (val > 256 ? 2 : 1)));
+      tft.unloadFont ();
+      i += (val > 256 ? 2 : 1);
+    } else if (Check_Location (val) == LAT2) {
+      tft.loadFont (latin_2);
+      tft.println (rain.substring (i, i + 3));
+      tft.unloadFont ();
+      i += 3;
+    } else if (Check_Location (val) == CJK1) {
+      tft.loadFont (cjk_1);
+      tft.println (rain.substring (i, i + 3));
+      tft.unloadFont ();
+      i += 3;
+    }
+    // } else if (Check_Location (val) == CJK2) {
+    //   tft.loadFont (cjk_2);
+    //   tft.println (rain.substring (i, i + 3));
+    //   tft.unloadFont ();
+    //   i += 3;
+    // } else if (Check_Location (val) == CJK3) {
+    //   tft.loadFont (cjk_3);
+    //   tft.println (rain.substring (i, i + 3));
+    //   tft.unloadFont ();
+    //   i += 3;
+    // } else if (Check_Location (val) == CJK4) {
+    //   tft.loadFont (cjk_4);
+    //   tft.println (rain.substring (i, i + 3));
+    //   tft.unloadFont ();
+    //   i += 3;
+    // } else if (Check_Location (val) == CJK5) {
+    //   tft.loadFont (cjk_5);
+    //   tft.println (rain.substring (i, i + 3));
+    //   tft.unloadFont ();
+    //   i += 3;
+    // } else if (Check_Location (val) == CJK6) {
+    //   tft.loadFont (cjk_6);
+    //   tft.println (rain.substring (i, i + 3));
+    //   tft.unloadFont ();
+    //   i += 3;
+    // } else if (Check_Location (val) == CJK7) {
+    //   tft.loadFont (cjk_7);
+    //   tft.println (rain.substring (i, i + 3));
+    //   tft.unloadFont ();
+    //   i += 3;
+    // } else if (Check_Location (val) == CJK8) {
+    //   tft.loadFont (cjk_8);
+    //   tft.println (rain.substring (i, i + 3));
+    //   tft.unloadFont ();
+    //   i += 3;
+    // } else if (Check_Location (val) == CJK9) {
+    //   tft.loadFont (cjk_9);
+    //   tft.println (rain.substring (i, i + 3));
+    //   tft.unloadFont ();
+    //   i += 3;
+    // } else if (Check_Location (val) == CJK10) {
+    //   tft.loadFont (cjk_10);
+    //   tft.println (rain.substring (i, i + 3));
+    //   tft.unloadFont ();
+    //   i += 3;
+    // } else if (Check_Location (val) == CJK11) {
+    //   tft.loadFont (cjk_11);
+    //   tft.println (rain.substring (i, i + 3));
+    //   tft.unloadFont ();
+    //   i += 3;
+    // }
+  }
+  // tft.loadFont(latin_1);
+  // tft.println(rain);
+  // tft.println (rain.substring (2));
+//   tft.println ("\nRYUSENKEI");
+  // tft.unloadFont();
 }
 
 //====================================================================================
@@ -140,22 +226,22 @@ void loop()
   //----------------------------------------------------------------------------
   // We can have any random mix of characters in the font
 
-  String test2 = "仝倀";//"雨のシンデレラ"; // Unicode 0x4EDD, 0x5000
+//   String test2 = "仝倀";//"雨のシンデレラ"; // Unicode 0x4EDD, 0x5000
 
-//   tft.loadFont(small_sample);
-  tft.loadFont(Unicode_Test_72);
+// //   tft.loadFont(small_sample);
+//   tft.loadFont(Unicode_Test_72);
 
-  tft.setRotation(1);
+//   tft.setRotation(1);
 
-// //   // Show all characters on screen with 2 second (2000ms) delay between screens
-  tft.showFont(2000); // Note: This function moves the cursor position!
+// // //   // Show all characters on screen with 2 second (2000ms) delay between screens
+//   tft.showFont(2000); // Note: This function moves the cursor position!
 
-  tft.fillScreen(bg);
-  tft.setCursor(0,0);
+//   tft.fillScreen(bg);
+//   tft.setCursor(0,0);
 
-  tft.setTextColor(TFT_CYAN, bg);
-  tft.println(test2);
-  tft.unloadFont();
+//   tft.setTextColor(TFT_CYAN, bg);
+//   tft.println(test2);
+//   tft.unloadFont();
 //   tft.loadFont (Latin_Hiragana_24);
 //   tft.println ("\nRYUSENKEI");
 
@@ -168,7 +254,7 @@ void loop()
 // //   Remove font parameters from memory to recover RAM
 //   tft.unloadFont();
 
-  delay(2000);
+  // delay(2000);
 
   //----------------------------------------------------------------------------
   // Latin and Hiragana font mix
